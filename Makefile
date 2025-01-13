072555f0b0ac7bf9ffb44c423135d58c518710e7
zabulyaka <zabulyaka@gmail.com>
2025-01-13 17:25:51 +0500

    Added prompt, welcome function

--- Makefile
+++ Makefile
@@ -9,3 +9,6 @@ build:
 
 package-install:
 	uv tool install dist/*.whl
+
+upgrade:
+	uv tool upgrade hexlet-code
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,11 @@
+from brain_games.cli import welcome_user
+
 def greet():
     print("Welcome to the Brain Games!")
 
 def main():
     greet()
+    welcome_user()
 
 if __name__ == "__main__":
     main()
